eca5954e8ce10b603e3f59d873e13a2a7aa49f1d
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2020-08-23 21:14:12 +0530

    More Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,27 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector('.header').style.top = '0px';
+    document.querySelector('.mainmenu').style.height = '100px';
+    document.querySelector('.logo').innerHTML = '<a href="index.html"><img src="assets/img/uds-hr-01.svg" alt="Ubiquity Design Studio Logo" width="35%" height="auto"></a>';
+    document.querySelector('.logo').style.padding = '29px 0';
+
+    var links = document.querySelectorAll('.menulinks');
+    console.log(links)
+    for (var i = 0; i < links.length; i ++) {
+      links[i].style.padding = '40px 20px';
+    }
+
   } else {
     document.querySelector('.header').style.top = '-50px';
+    document.querySelector('.mainmenu').style.height = '50px'
+    document.querySelector('.logo').innerHTML = '<a href="index.html"><img src="assets/img/uds-hr-02.svg" alt="Ubiquity Design Studio Logo" width="50%" height="auto"></a>';
+    document.querySelector('.logo').style.padding = '14px 0';
+
+    var links = document.querySelectorAll('.menulinks');
+    for (var i = 0; i < links.length; i ++) {
+      links[i].style.padding = '15px 20px';
+    }
+    
   }
   prevScrollpos = currentScrollPos;
 }
